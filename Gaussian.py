e7d408fba4bdabc07b6220a279abe9385d988fd5
TANAKA-Makoto <justice.23.t.m@gmail.com>
2019-03-04 16:17:34 +0900

    can get args

--- Gaussian.py
+++ Gaussian.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 import sys
+import argparse
 import cv2
 import numpy as np
 
-name = sys.argv[0]
-img_path = sys.argv[1]
-dst_path = sys.argv[2]
+parser = argparse.ArgumentParser()
+parser.add_argument("img_path", type=str)
+parser.add_argument("dst_path", type=str)
+parser.add_argument("--window", type=tuple)
+parser.add_argument("--sigmaX", type=int)
+parser.add_argument("--sigmaY", type=int)
+
+
+args = parser.parse_args()
+img_path = args.img_path
+dst_path = args.dst_path
+if args.window:
+    window = args.window
+else:
+    window = (125, 125)
+
+if args.sigmaX:
+    sigmaX = args.sigmaX
+else:
+    sigmaX = 1000
+
+if args.sigmaY:
+    sigmaY = args.sigmaY
+else:
+    sigmaY = 1000
 
 img = cv2.imread(img_path)
 print(f'{img_path} is loaded')
