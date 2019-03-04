d94b677d861d901122f74a739650975099825d17
TANAKA-Makoto <justice.23.t.m@gmail.com>
2019-03-04 15:20:34 +0900

    update Gaussian.py
    console log print

--- Gaussian.py
+++ Gaussian.py
@@ -1,26 +1,17 @@
+#!/usr/bin/python3
 import cv2
 import numpy as np
-#from matplotlib import pyplot as plt
 
-img = cv2.imread('map_1_scan_allpoint_50m_cleaned.png')
+img_path = './img/map_1_scan_allpoint_50m_cleaned.png'
+dst_path = 'map_1_scan_allpoint_50m_cost125_1000_new.png'
+img = cv2.imread(img_path)
+print(f'{img_path} is loaded')
 
-dst = cv2.GaussianBlur(img,(125,125),sigmaX=1000,sigmaY=1000)
-# dst = cv2.GaussianBlur(img,(35,35),sigmaX=500,sigmaY=500)
+dst = cv2.GaussianBlur(img, (125, 125), sigmaX=1000, sigmaY=1000)
+print('filter done')
 
+dst = np.minimum(dst, img)
+print('mixing done')
 
-#plt.subplot(121),plt.imshow(img),plt.title('Original')
-#plt.xticks([]), plt.yticks([])
-#plt.subplot(122),plt.imshow(dst),plt.title('Gaussian')
-#plt.xticks([]), plt.yticks([])
-
-#plt.show()
-
-print('Writing...')
-
-# for i in range(dst.shape[0]):
-#     for j in range(dst.shape[1]):
-#         for k in range(dst.shape[2]):
-#             dst[i][j][k] = min(dst[i][j][k],img[i][j][k])
-dst = np.minimum(dst,img)
-
-cv2.imwrite('map_1_scan_allpoint_50m_cost125_1000_new.png', dst)
+cv2.imwrite(dst_path, dst)
+print(f'output to {dst_path}')
